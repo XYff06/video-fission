d709db76fbd2607dc09cb9399d118b31c92595af
XYff06 <2686673952@qq.com>
2026-03-31 14:43:01 +0800

    cc

--- backend/app.py
+++ backend/app.py
@@ -140,5 +140,13 @@ def download_clip(job_id: str, video_id: str, filename: str):
     return send_from_directory(target_dir, filename, as_attachment=False)
 
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "http://127.0.0.1:5173"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
